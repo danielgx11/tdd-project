d0934c56d58a1750606a5e74525a0ded4da0aa17
danielgx11 <danielgx@ufmg.br>
2023-05-04 23:04:09 -0300

    Um pouco de arquitetura web: MVC, URLs e View

--- lists/tests.py
+++ lists/tests.py
@@ -6,4 +6,4 @@ class HomePageTest(TestCase):
 
     def test_root_url_resolves_to_home_page_view(self):
         found = resolve('/')  
-        self.assertEqual(found.func, home_page)
+        self.assertEqual(found.func, home_page)  
