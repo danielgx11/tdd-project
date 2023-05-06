fcffd34ac4f59dd872f1e1268ae6d121a3363a2b
danielgx11 <danielgx@ufmg.br>
2023-05-05 21:43:05 -0300

    Processando Requisição POST

--- lists/tests.py
+++ lists/tests.py
@@ -9,10 +9,6 @@ class HomePageTest(TestCase):
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
-        self.assertTrue(
-        any(row.text == '1: Estudar testes funcionais' for row in rows),
-        "New to-do item did not appear in table"
-        )
         
     def test_home_page_returns_correct_html(self):
         response = self.client.get('/')  
@@ -22,4 +18,8 @@ class HomePageTest(TestCase):
         self.assertIn('<title>To-Do lists</title>', html)
         self.assertTrue(html.strip().endswith('</html>'))
 
-        self.assertTemplateUsed(response, 'home.html') 
+        self.assertTemplateUsed(response, 'home.html')
+
+    def test_can_save_a_POST_request(self):
+        response = self.client.post('/', data={'item_text': 'A new list item'})
+        self.assertIn('A new list item', response.content.decode())
